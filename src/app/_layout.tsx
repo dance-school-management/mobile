4b03aea54fa74991fea90cb728f65e03180db96f
Dariusz Cebula <darko2448@gmail.com>
2025-05-29 01:41:15 +0200

    Fix gluestack

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,13 +1,16 @@
 import { Stack } from 'expo-router';
 import React from 'react';
 import '../../global.css'
+import { GluestackUIProvider } from '@/components/ui/gluestack-ui-provider';
 
 export default function Layout() {
   return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="index" />
-      <Stack.Screen name="register" />
-      <Stack.Screen name="(drawer)" />
-    </Stack>
+    <GluestackUIProvider>
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="index" />
+        <Stack.Screen name="register" />
+        <Stack.Screen name="(drawer)" />
+      </Stack>
+    </GluestackUIProvider>
   );
 }
